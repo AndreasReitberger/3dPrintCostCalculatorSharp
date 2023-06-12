a75129b37fff0d6bad60f526d8b75c74f1e77992
Andreas <andy.reitberger@gmail.com>
2023-06-12 20:00:12 +0200

    Add methods for `File3d` database fetch and save
    This commit adds the needed events and methods to save and load `File3d` objects to the database.
    
    Fixed #41

---
+++ source/3dPrintCalculatorLibrary/Utilities/PrintFileProcessingInfo.cs
@@ -0,0 +1,60 @@
+﻿using AndreasReitberger.Print3d.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace AndreasReitberger.Print3d.Utilities
+{
+    public partial class PrintFileProcessingInfo : ObservableObject
+    {
+        #region Properties
+
+        [ObservableProperty]
+        Guid id;
+
+        [ObservableProperty]
+        string fileName;
+
+        [ObservableProperty]
+        string filePath;
+
+        [ObservableProperty]
+        double progress = 0;
+        partial void OnProgressChanged(double value)
+        {
+            IsDone = value >= 100;
+        }
+
+        [ObservableProperty]
+        bool isDone = false;
+
+        [ObservableProperty]
+        double printDuration = 0;
+
+        [ObservableProperty]
+        double volume = 0;
+
+        #endregion
+
+        #region Ctor
+        public PrintFileProcessingInfo()
+        {
+            Id = Guid.NewGuid();
+        }
+        #endregion
+
+        #region Methods
+
+        public File3d ToFile()
+        {
+            return new File3d()
+            {
+                Name = FileName,
+                FilePath = FilePath,
+                PrintTime = PrintDuration,
+                Volume = Volume,
+            };
+        }
+
+        #endregion
+    }
+}
